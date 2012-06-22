8709c4cab59b9e250c958027a400937cce8a4ea4
ys0 <yoshi00mail@gmail.com>
2012-06-23 01:34:03 +0200

    More useul methods
    I implemented the autodetect of primary key and the create/drop methods
    for databases (and a method to switch database) and tables, createTable
    needs to be hard tested but i have to ultimate it because it will help a
    lot on the database setup of any applications (i mean an auto database
    creation as wordpress)

--- index.php
+++ index.php
@@ -13,54 +13,85 @@
 	 * mysql_select_db('test', $link);
 	 */
 	
-	$myPulse = new Pulse(Array(
-			'host' => 'localhost',
-			'username' => '',
-			'password' => '',
-			'database' => 'test',
-			'table' => 'people',
-			//'link' => $link, 	
-			//'column' => 'name',
-			//'value' => 'ys0'
-	));
+// 	$myPulse = new Pulse(Array(
+// 			'host' => 'localhost',
+// 			'username' => '',
+// 			'password' => '',
+// 			'database' => 'test',
+// 			'table' => 'people',
+// 			//'link' => $link, 	
+// 			//'column' => 'name',
+// 			//'value' => 'ys0'
+// 	));
 	
-	$myPulse->load('name', 'ys0');
+// 	$myPulse->load('name', 'ys0');
+// 	debug($myPulse);
 	
-	debug($myPulse);
+// 	echo $myPulse->data['name'] . ' is ' . $myPulse->data['age'] . ' years old.';
+// 	debug($myPulse);
 	
-	echo $myPulse->data['name'] . ' is ' . $myPulse->data['age'] . ' years old.';
-	
-	debug($myPulse);
-	
-	$myPulse->data['age'] = rand(10, 50);
-	$myPulse->update();
-	
-	debug($myPulse);
+// 	$myPulse->data['age'] = rand(10, 50);
+// 	$myPulse->update();
+// 	debug($myPulse);
 
-	$newUser = new Pulse(Array(
-			'link' => $myPulse->getLink(),
-			'table' => 'people'
+// 	$newUser = new Pulse(Array(
+// 			'link' => $myPulse->getLink(),
+// 			'table' => 'people'
+// 	));
+	
+// 	$newUser->data['name'] = 'one';
+// 	$newUser->data['age'] = 27;
+// 	$newUser->create();
+// 	$newUser->create(); //ops i create 2 record
+// 	debug($newUser);
+// 	print_r($newUser->getAll());
+	
+// 	$newUser->load('name', 'one'); //it load only the first record found
+// 	$newUser->delete();
+// 	debug($newUser);
+// 	print_r($newUser->getAll());
+	
+// 	$data = $pulse1->join($pulse2, 'field');
+// 	$data = $pulse1->join(Array($pulse2, $pulse3), Array('field1', 'field2'));
+// 	debug($newUser);
+	
+// 	$newUser->createDatabase('ganja');
+// 	debug($newUser);
+	
+// 	$newUser->dropDatabase('puzza');
+// 	debug($newUser);
+
+	$myDB = new Pulse(Array(
+		'host' => 'localhost',
+		'username' => '',
+		'password' => '',
+		'database' => 'test'
+	));
+
+	$myDB->createTable('people2', Array(
+			Array(
+					'name' => 'id',
+					'type' => 'INT',
+					'option' => '11',
+					'auto_increment' => true,
+					'null' => false,
+					'primary_key' => true
+			),
+			Array(
+					'name' => 'name',
+					'type' => 'VARCHAR',
+					'option' => '30'
+			),
+			Array(
+					'name' => 'age',
+					'type' => 'INT',
+					'option' => '3'
+			),
 	));
 	
-	$newUser->data['name'] = 'one';
-	$newUser->data['age'] = 27;
+	debug($myDB);
 	
-	$newUser->create();
-	$newUser->create(); //ops i create 2 record
+	$myDB->dropTable('people2');
 	
-	debug($newUser);
-	
-	print_r($newUser->getAll());
-	
-	$newUser->load('name', 'one'); //it load only the first record found
-	$newUser->delete();
-	
-	debug($newUser);
-	
-	print_r($newUser->getAll());
-	
-	/*
-	 * $data = $pulse1->join($pulse2, 'field');
-	 * $data = $pulse1->join(Array($pulse2, $pulse3), Array('field1', 'field2'));
-	 */
+	debug($myDB);
 ?>
